d0c94549c0f07b9204d84aaf8a92b89b42e475ec
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-06-12 01:14:03 +0530

    Update useLogin.js

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { useAuthContext } from "../context/AuthContext";
 
-const BASE_URL = "https://messenger-k0ny.onrender.com/api" || "http://localhost:5000/api";
+const BASE_URL = "https://messenger-k0ny.onrender.com/api";
 
 const useLogin= ()=>{
     const[loading,setLoading]=useState(false);
