33fab92762f1dcf12d98a40c3a1d9a4b22370cf1
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-06-11 23:46:29 +0530

    Update useSingup.js

--- frontend/src/hooks/useSingup.js
+++ frontend/src/hooks/useSingup.js
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import toast from "react-hot-toast";
 import { useAuthContext } from "../context/AuthContext";
+const BASE_URL="https://messenger-k0ny.onrender.com" || "http://localhost:5000"
 const useSignup=() =>{
     const[loading,setLoading]=useState(false);
     const {setAuthUser}=useAuthContext();
