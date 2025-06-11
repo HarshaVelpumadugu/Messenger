62cf9794a107d42c52414e2ec719ca63d377c64b
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-06-12 00:16:22 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,7 +6,8 @@ const app=express();
 const server=http.createServer(app);
 const io=new Server(server,{
     cors:{
-        origin:["http://localhost:3000"],
+        //origin:["http://localhost:3000"],
+        origin:["https://messenger-frontend-kazz.onrender.com"],
         methods:["GET","POST"]
     }
 });
