77d10a09760d9a6b743961aa2e109b3c526f7150
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-06-11 23:30:14 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
     port:3000,
     proxy:{
       "/api":{
-        target:'http://localhost:5000',
+        //target:'http://localhost:5000',
+        target:'https://messenger-k0ny.onrender.com'
         changeOrigin: true, 
       },
     },
