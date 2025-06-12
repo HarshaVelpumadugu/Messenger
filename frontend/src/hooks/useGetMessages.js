7966db3f709706215d717eebf5a1a0368ec959cd
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-06-12 10:27:56 +0530

    Update useGetMessages.js

--- frontend/src/hooks/useGetMessages.js
+++ frontend/src/hooks/useGetMessages.js
@@ -2,7 +2,7 @@ import {useState,useEffect} from 'react'
 import useConversation from '../zustand/useConversation';
 import toast from 'react-hot-toast';
 
-const BASE_URL = "https://messenger-j1ha.onrender.com/api";
+const BASE_URL = "https://messenger-backend-1-wfgx.onrender.com/api";
 export const useGetMessages = () => {
   const [loading,setLoading]=useState(false);
   const {messages,setMessages,selectedConversation}=useConversation();
