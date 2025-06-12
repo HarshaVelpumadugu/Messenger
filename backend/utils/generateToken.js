14b0c8d6c2c9113f9dc129eb931c52c36e4941a6
HarshaVelpumadugu <136341556+HarshaVelpumadugu@users.noreply.github.com>
2025-06-12 11:21:16 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,14 +1,16 @@
 import jwt from "jsonwebtoken";
 
-const generateTokenAndSetCookie=(userId,res)=>{
-    const token=jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:'15d'
-    })
-    res.cookie("jwt",token,{
-        maxAge: 15 * 24 * 60 * 60 * 1000,
-        httpOnly:true,
-        sameSite:"strict",
-        secure:process.env.NODE_ENV !== "development",
-    });
+const generateTokenAndSetCookie = (userId, res) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+
+  res.cookie("jwt", token, {
+    maxAge: 15 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    sameSite: "None",   
+    secure: true,           
+  });
 };
+
 export default generateTokenAndSetCookie;
